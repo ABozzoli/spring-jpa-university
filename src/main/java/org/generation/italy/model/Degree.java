0e6e7f1993334134b5914133dbec2e4ab5c7659e
ABozzoli <alessandrobozzoli92@gmail.com>
2022-01-07 18:19:18 +0100

    Aggiunta entity Degree

---
+++ src/main/java/org/generation/italy/model/Degree.java
@@ -0,0 +1,74 @@
+package org.generation.italy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="degrees")
+public class Degree {
+
+	//attributi
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@ManyToOne(fetch=FetchType.LAZY) // fetch è opzionale e definisce cosa prende (LAZY non fa join)
+	@JoinColumn(name="department_id")
+	private Department department;
+	
+	private String name;
+	private String level;
+	private String address;
+	private String email;
+	private String website;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	
+}
