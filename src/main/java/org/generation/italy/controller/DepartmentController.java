8a5b1f25be7bfa29a14b2b70ae1d1682fbe0511e
ABozzoli <alessandrobozzoli92@gmail.com>
2022-01-07 17:32:59 +0100

    Aggiunto link detail

--- src/main/java/org/generation/italy/controller/DepartmentController.java
+++ src/main/java/org/generation/italy/controller/DepartmentController.java
@@ -1,24 +1,34 @@
 package org.generation.italy.controller;
 
+import org.generation.italy.model.Department;
 import org.generation.italy.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class DepartmentController {
 
-	@Autowired
-	private DepartmentRepository repository;
+	@Autowired // ogni volta che crea un'istanza controller deva anche iniettarle da repo
+	private DepartmentRepository repo;
 
 	@GetMapping
-	public String department(Model model) {
-		model.addAttribute("departments", repository.findAll(Sort.by("name")));
+	public String departmentList(Model model) {
+		model.addAttribute("list", repo.findAll(Sort.by("name")));
 		return "departments";
+	}
+	
+	@GetMapping("/department/{id}")
+	public String departmentDetail(Model model, @PathVariable Integer id) {
+		Department d = repo.getById(id);
+		
+		model.addAttribute("department", d);
+		return "detail";
 	}
 	
 }
